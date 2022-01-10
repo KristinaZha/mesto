45c8682239c3ef2d2918337844f3b216853e50c4
Kristina <kristinka288@gmail.com>
2022-01-10 14:49:54 +0300

    practika5.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,118 +1,140 @@
-const popupElement = document.querySelector(".popup");
-const closeButton = document.querySelector(".popup__close-button");
-const profileButton = document.querySelector(".profile__edit-button");
-let formElement = document.querySelector(".form");
-let userName = document.querySelector(".profile__title");
-let userText = document.querySelector(".profile__subtitle");
-let FormName = document.querySelector(".form__type_text_name");
-let FormRole = document.querySelector(".form__type_text_role");
-const addElement = document.querySelector(".popup-add");
-const closeButtonAdd = document.querySelector(".popup-add__close-button");
-
+const popupElement = document.querySelector(".popup")
+const buttonClose = document.querySelector(".popup__close-button")
+const profileButton = document.querySelector(".profile__edit-button")
+const formElement = document.querySelector(".form")
+const userName = document.querySelector(".profile__title")
+const userText = document.querySelector(".profile__subtitle")
+const FormName = document.querySelector(".form__type_text_name")
+const FormRole = document.querySelector(".form__type_text_role")
+const elementAdd = document.querySelector(".popup-add")
+const buttonCloseAdd = document.querySelector(".popup-add__close-button")
+const popupFoto = document.querySelector(".popup-foto")
+// добавление карточки через form
+const buttonAdd = document.querySelector(".profile__button")
+const elementPic = document.querySelector(".element__pic")
+const elementTitle = document.querySelector(".element__title")
+const name = document.querySelector(".form-add__type_text_name")
+const hrefPic = document.querySelector(".form-add__type_text_role")
+const cardAdd = document.querySelector(".form-add")
+//ф-я открытия попапа добавления картинки
 function openPopupAdd() {
-  addElement.classList.add("popup-add_opened");
- 
+  elementAdd.classList.add("popup-add_opened")
 }
+//ф-я закрытия попапа картинки
 function closePopupAdd() {
-  addElement.classList.remove("popup-add_opened");
-
+  elementAdd.classList.remove("popup-add_opened")
 }
-
+//ф-я открытия попапа редактирования профайла
 function openPopup() {
-  popupElement.classList.add("popup_opened");
-  FormName.value = userName.textContent;
-  FormRole.value = userText.textContent;
-
+  popupElement.classList.add("popup_opened")
+  FormName.value = userName.textContent
+  FormRole.value = userText.textContent
 }
-
+//ф-я закрытия попапа редактирования профайла
 function closePopup() {
-  popupElement.classList.remove("popup_opened");
+  popupElement.classList.remove("popup_opened")
 }
-
+//форма изменений профайла
 function formSubmitHandler(evt) {
-  evt.preventDefault();
-  userName.textContent = FormName.value;
-  userText.textContent = FormRole.value;
-  closePopup();
+  evt.preventDefault()
+  userName.textContent = FormName.value
+  userText.textContent = FormRole.value
+  closePopup()
+}
+//форма добавления картинки
+function formAddSubmitHandler(evt) {
+  evt.preventDefault()
+  const inputName = Name.value
+  const inputImage = hrefPic.value
+  const newCard = createDomNode({ name: inputName, link: inputImage })
+  elements.prepend(newCard)
+  closePopupAdd()
 }
 
-closeButtonAdd.addEventListener("click", closePopupAdd)
-profileButton.addEventListener("click", openPopup)
-closeButton.addEventListener("click", closePopup)
-formElement.addEventListener("submit", formSubmitHandler)
-
-
-// массив 
+// массив
 const initialCards = [
-  
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: "Архыз",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-const elements = document.querySelector('.elements');
-const element = document.querySelector('.element');
-const template = document.querySelector('.template'); 
+    name: "Байкал",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+]
+const elements = document.querySelector(".elements")
+const element = document.querySelector(".element")
+const template = document.querySelector(".template")
+const imagePopup = document.querySelector(".popup-foto__image")
+const captionPopup = document.querySelector(".popup-foto__caption")
 //динамическое добавление карточек на страницу тег template
 const createDomNode = (item) => {
-  const elementTemplate = template.content.querySelector('.element').cloneNode(true);
-  elementTemplate.querySelector('.element__pic').src = item.link;
-  elementTemplate.querySelector('.element__title').textContent = item.name;
-  return elementTemplate;
-}
-const result = initialCards.map((item) => {
-  return createDomNode(item);
-});
-elements.append(...result);
-const buttonDeleteCard = elementTemplate.querySelector(".element__delete");
-const buttonLike = elementTemplate.querySelector(".element__like");
-function deleteCard() {
-  elementTemplate.remove();
+  const elementTemplate = template.content
+    .querySelector(".element")
+    .cloneNode(true)// клонирование всего содержимого элемента
+  elementTemplate.querySelector(".element__pic").src = item.link// ссылка на картинку
+  elementTemplate.querySelector(".element__title").textContent = item.name// имя из заголовка
+  const buttonDeleteCard = elementTemplate.querySelector(".element__delete") //удаление карточки
+  const buttonLike = elementTemplate.querySelector(".element__like") // лайк карточки
+  function deleteCard() {//ф-я удаление карточки
+    elementTemplate.remove()
   }
-buttonDeleteCard.addEventListener(".click", deleteCard);
-function likeCard() {
-  buttonLike.classList.toggle(".element__like-activ");
-}
-buttonLike.addEventListener("click", likeCard)
-// добавление карточки через form
-const buttonAdd = document.querySelector(".profile__button");
-let elementPic = document.querySelector(".element__pic");
-let elementTitle = document.querySelector(".element__title");
-let Name = document.querySelector(".form-add__type_text_name");
-let hrefPic = document.querySelector(".form-add__type_text_role");
-let addCard = document.querySelector(".form-add");
-function formAddSubmitHandler(evt) {
-  evt.preventDefault();
-  const inputName = Name.value;
-  const inputImage = hrefPic.src;
-  const newCard = createDomNode({name:inputName, link:hrefPic});
-  elements.prepend(newCard);
-  closePopupAdd();
+  function likeCard() {// ф-я лайк карточки
+    buttonLike.classList.toggle("element__like-activ")
+  }
+
+  elementTemplate
+    .querySelector(".element__pic")
+    .addEventListener("click", function (evt) {// ф-я открытия попапа с картинкой
+      popupFoto.classList.add("popup-foto_opened")
+      imagePopup.src = item.link
+      captionPopup.textContent = item.name
+    })
+  buttonLike.addEventListener("click", likeCard) //слушатель лайка
+  buttonDeleteCard.addEventListener("click", deleteCard) //слушатель удаления карточки
+
+  elementTemplate.prepend
+  return elementTemplate
 }
 
+const result = initialCards.map((item) => {
+  return createDomNode(item)
+})
+elements.append(...result)
+
+const fotoClose = document.querySelector(".popup-foto__close-button") // кнопка закрытие попапа с  фото
+function closePopupFoto() {
+  popupFoto.classList.remove("popup-foto_opened")
+}
+// слушатели
+fotoClose.addEventListener("click", closePopupFoto)
+buttonCloseAdd.addEventListener("click", closePopupAdd)
+profileButton.addEventListener("click", openPopup)
+buttonClose.addEventListener("click", closePopup)
+formElement.addEventListener("submit", formSubmitHandler)
+profileButton.addEventListener("click", openPopup)
+buttonClose.addEventListener("click", closePopup)
 buttonAdd.addEventListener("click", openPopupAdd)
-addCard.addEventListener("submit", formAddSubmitHandler)
-
-
-
-
+cardAdd.addEventListener("submit", formAddSubmitHandler)
