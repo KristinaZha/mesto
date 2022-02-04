79803ece5aa193e47369d849594ccc6094fb49ea
Kristina <kristinka288@gmail.com>
2022-02-04 13:30:54 +0300

    practika6.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,85 +1,35 @@
+const popup = document.querySelectorAll(".popup")
 const popupProfile = document.querySelector(".popup-profile")
-const buttonClose = document.querySelector(".popup-profile__close-button")//
+const buttonClose = document.querySelector(".popup-profile__close-button")
+
 const profileButton = document.querySelector(".profile__edit-button")
-const formElement = document.querySelector(".form")
 const userName = document.querySelector(".profile__title")
 const userText = document.querySelector(".profile__subtitle")
+const buttonAdd = document.querySelector(".profile__button")
+
+const formElement = document.querySelector(".form")
 const userNameInput = document.querySelector(".form__input_name")
 const formInputRole = document.querySelector(".form__input_role")
+
 const popupAddCard = document.querySelector(".popup-add")
-const buttonCloseAdd = document.querySelector(".popup-add__close-button")//
+const buttonCloseAdd = document.querySelector(".popup-add__close-button")
+
 const popupPhoto = document.querySelector(".popup-foto")
+const buttonClosePhoto = document.querySelector(".popup-foto__close-button") // кнопка закрытие попапа с  фото
+const imagePopup = document.querySelector(".popup-foto__image")
+const captionPopup = document.querySelector(".popup-foto__caption")
+
 // добавление карточки через form
-const buttonAdd = document.querySelector(".profile__button")
 const elementPic = document.querySelector(".element__pic")
 const elementTitle = document.querySelector(".element__title")
+const elements = document.querySelector(".elements")
+const element = document.querySelector(".element")
+const template = document.querySelector(".template")
+
 const name = document.querySelector(".form__input_text")
 const hrefPic = document.querySelector(".form__input_image")
 const formAddCard = document.querySelector(".form-card")
 const buttonSubmit = document.querySelector(".form__type-submit")
-const popup = document.querySelectorAll('.popup')
-
-//ф-я открытия попапа добавления картинки
-function openPopupAddCard() {
-  popupAddCard.classList.add("popup_opened")
-  document.addEventListener('keydown', closePopupEscape);
-};
-
-//ф-я закрытия попапа картинки
-function closePopupAddCard() {
-  popupAddCard.classList.remove("popup_opened")
-  document.removeEventListener('keydown', closePopupEscape);
-}
-
-//ф-я открытия попапа редактирования профайла
-function openPopupProfile() {
-  popupProfile.classList.add("popup_opened")
-  userNameInput.value = userName.textContent
-  formInputRole.value = userText.textContent
-  document.addEventListener('keydown', closePopupEscape)
-
-}
-//ф-я закрытия попапа редактирования профайла
-function closePopupProfile() {
-  popupProfile.classList.remove("popup_opened")
-  document.removeEventListener('keydown', closePopupEscape);
-
-}
-//ф-я закрытия попапов esc
-function closePopupEscape(evt) {
-  if (evt.key === 'Escape') {
-    closePopupAddCard(document.querySelector('popup_opened'));
-    closePopupProfile(document.querySelector('popup_opened'));
-    closePopupPhoto(document.querySelector('popup_opened'));
-  }
-};
-//ф-я закрытия при клике на оверлей
-popup.forEach((popup) => {
-  popup.addEventListener('click', (evt) => {
-    if (evt.target.classList.contains('popup_opened')) {
-      closePopupAddCard(document.querySelector('popup_opened'));
-      closePopupProfile(document.querySelector('popup_opened'));
-      closePopupPhoto(document.querySelector('popup_opened'));
-    }
-  })
-})
-//форма изменений профайла
-function formSubmitHandler(evt) {
-  evt.preventDefault()
-  userName.textContent = userNameInput.value
-  userText.textContent = formInputRole.value
-  closePopupProfile()
-}
-//форма добавления картинки
-function CardAddSubmitHandler(evt) {
-  evt.preventDefault()
-  const inputName = name.value
-  const inputImage = hrefPic.value
-  const newCard = createDomNode({ name: inputName, link: inputImage })
-  elements.prepend(newCard);
-  formAddCard.reset();
-  closePopupAddCard();
-}
 
 // массив
 const initialCards = [
@@ -114,37 +64,33 @@ const initialCards = [
       "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ]
-const elements = document.querySelector(".elements")
-const element = document.querySelector(".element")
-const template = document.querySelector(".template")
-const imagePopup = document.querySelector(".popup-foto__image")
-const captionPopup = document.querySelector(".popup-foto__caption")
+
 //динамическое добавление карточек на страницу тег template
 const createDomNode = (item) => {
   const elementTemplate = template.content
     .querySelector(".element")
-    .cloneNode(true)// клонирование всего содержимого элемента
-  elementTemplate.querySelector(".element__pic").src = item.link// ссылка на картинку
-  elementTemplate.querySelector(".element__title").textContent = item.name// имя из заголовка
-  const buttonDeleteCard = elementTemplate.querySelector(".element__delete") //удаление карточки
-  const buttonLike = elementTemplate.querySelector(".element__like") // лайк карточки
-  function deleteCard() {//ф-я удаление карточки
-    elementTemplate.remove()
-  }
-  function likeCard() {// ф-я лайк карточки
-    buttonLike.classList.toggle("element__like-activ")
-  }
-
+    .cloneNode(true) // клонирование всего содержимого элемента
+  elementTemplate.querySelector(".element__pic").src = item.link // ссылка на картинку
+  elementTemplate.querySelector(".element__title").textContent = item.name // имя из заголовка
   elementTemplate
     .querySelector(".element__pic")
-    .addEventListener("click", function (evt) {// ф-я открытия попапа с картинкой
-      popupPhoto.classList.add("popup_opened")
-      document.addEventListener('keydown', closePopupEscape)
+    .addEventListener("click", function () {
+      // ф-я открытия попапа с картинкой
+      openPopup(popupPhoto)
       imagePopup.src = item.link
       captionPopup.textContent = item.name
     })
-  buttonLike.addEventListener("click", likeCard) //слушатель лайка
-  buttonDeleteCard.addEventListener("click", deleteCard) //слушатель удаления карточки
+
+  const buttonDeleteCard = elementTemplate.querySelector(".element__delete") //удаление карточки
+  buttonDeleteCard.addEventListener("click", function () {
+    elementTemplate.remove()
+  })
+
+  const buttonLike = elementTemplate.querySelector(".element__like") // лайк карточки
+  buttonLike.addEventListener("click", function () {
+    buttonLike.classList.toggle("element__like-activ")
+  })
+
   elementTemplate.prepend
   return elementTemplate
 }
@@ -154,32 +100,75 @@ const result = initialCards.map((item) => {
 })
 elements.append(...result)
 
-const buttonClosePhoto = document.querySelector(".popup-foto__close-button") // кнопка закрытие попапа с  фото
-
-
-//ф-я закрытия попапа с фото
-function closePopupPhoto() {
-  popupPhoto.classList.remove("popup_opened");
-  document.removeEventListener('keydown', closePopupEscape);
+// ф-я открытия попапов
+function openPopup(popup) {
+  popup.classList.add("popup_opened")
+  document.addEventListener("keydown", closePopupEscape)
 }
 
+// ф-я закрытия попапов
+function closePopup(popup) {
+  popup.classList.remove("popup_opened")
+  document.removeEventListener("keydown", closePopupEscape)
+}
 
-// слушатели
-buttonClosePhoto.addEventListener("click", closePopupPhoto)//
-buttonCloseAdd.addEventListener("click", closePopupAddCard)//
-profileButton.addEventListener("click", openPopupProfile)
-buttonClose.addEventListener("click", closePopupProfile)//
-formElement.addEventListener("submit", formSubmitHandler)
-buttonAdd.addEventListener("click", openPopupAddCard)
-formAddCard.addEventListener("submit", CardAddSubmitHandler)
+//перебор
+popup.forEach((popup) => {
+  popup.addEventListener("click", (e) => {
+    if (e.target.classList.contains("popup_opened")) {
+      closePopup(popup)
+    }
+    if (e.target.classList.contains("popup__close-button")) {
+      closePopup(popup)
+    }
+  })
+})
+//ф-я закрытия попапов esc
+function closePopupEscape(evt) {
+  if (evt.key === "Escape") closePopup(document.querySelector(".popup_opened"))
+}
 
+//ф-я редактирования профайла
+function openProfile() {
+  userNameInput.value = userName.textContent
+  formInputRole.value = userText.textContent
+  openPopup(popupProfile)
+}
 
+//форма изменений профайла
+function handleSubmitProfileForm(evt) {
+  evt.preventDefault()
+  userName.textContent = userNameInput.value
+  userText.textContent = formInputRole.value
+  closePopup(popupProfile)
+}
 
+//форма добавления картинки
+function handleSubmitCardForm(evt) {
+  evt.preventDefault()
+  const inputName = name.value
+  const inputImage = hrefPic.value
+  const newCard = createDomNode({ name: inputName, link: inputImage })
+  elements.prepend(newCard)
+  closePopup(popupAddCard)
+  formAddCard.reset()
+}
 
+// слушатели профайла
 
+profileButton.addEventListener("click", () => openPopup(popupProfile))
 
+buttonClose.addEventListener("click", () => closePopup(popupProfile))
 
+profileButton.addEventListener("click", openProfile)
 
+// слушатели добавления картинки
 
+buttonAdd.addEventListener("click", () => openPopup(popupAddCard))
 
+buttonCloseAdd.addEventListener("click", () => closePopup(popupAddCard))
 
+buttonClosePhoto.addEventListener("click", () => closePopup(popupPhoto))
+
+formElement.addEventListener("submit", handleSubmitProfileForm)
+formAddCard.addEventListener("submit", handleSubmitCardForm)
